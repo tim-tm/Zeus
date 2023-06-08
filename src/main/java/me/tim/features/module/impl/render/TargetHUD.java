08e739f682424eeda4b08ee8c56de2e8a201c173
Web-Tim <teichmanntim@outlook.de>
2023-06-08 12:55:01 +0200

    migrate to Maven

--- src/main/java/me/tim/features/module/impl/render/TargetHUD.java
+++ src/main/java/me/tim/features/module/impl/render/TargetHUD.java
@@ -8,6 +8,8 @@ import me.tim.features.module.Category;
 import me.tim.features.module.Module;
 import me.tim.features.module.impl.combat.KillAura;
 import me.tim.ui.click.settings.impl.BooleanSetting;
+import me.tim.ui.click.settings.impl.ModeSetting;
+import me.tim.util.common.EnumUtil;
 import me.tim.util.common.MathUtil;
 import me.tim.util.render.RenderUtil;
 import net.minecraft.client.renderer.GlStateManager;
@@ -19,6 +21,7 @@ import java.awt.*;
 
 public class TargetHUD extends Module {
     private BooleanSetting projectedSetting;
+    private ModeSetting modeSetting;
 
     private KillAura killAuraModule;
     private float x, y, width, height, healthPerc;
@@ -30,6 +33,7 @@ public class TargetHUD extends Module {
 
     @Override
     protected void setupSettings() {
+        this.settings.add(this.modeSetting = new ModeSetting("Mode", "Different TargetHUD Styles!", TargetHudMode.values(), TargetHudMode.BULKY));
         this.settings.add(this.projectedSetting = new BooleanSetting("Projected", "TargetHUD appears Projected!", false));
     }
 
@@ -70,21 +74,34 @@ public class TargetHUD extends Module {
     }
 
     private void drawTargetHUD() {
+        TargetHudMode mode = (TargetHudMode) EnumUtil.fromName(this.modeSetting.getCurrentMode().getName(), TargetHudMode.values());
+        if (mode == null) return;
+
         this.healthPerc = MathUtil.interpolate(this.healthPerc, MathUtil.percentage(this.killAuraModule.getCurrTarget().getHealth(), this.killAuraModule.getCurrTarget().getMaxHealth()), 0.1).floatValue();
         healthPerc = MathHelper.clamp_float(healthPerc, 0, 1);
         int col = (int) (255 * healthPerc);
 
-        RenderUtil.drawRoundedRect(this.x, this.y, this.x + this.width, this.y + this.height, 2f, new Color(35, 35, 35));
-
-        Statics.getFontRenderer().drawString(this.killAuraModule.getCurrTarget().getName(), (int) (this.x + 10), (int) (this.y + 10), -1);
-
-        bloomBuffer.framebufferClear();
-        bloomBuffer.bindFramebuffer(true);
-        RenderUtil.drawRoundedRect(this.x + 10, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT, this.x + (this.width - 10) * healthPerc, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT + this.height / 8, 2f, new Color(255 - col, col, 0));
-        bloomBuffer.unbindFramebuffer();
-        RenderUtil.drawBloom(bloomBuffer.framebufferTexture, 15, 1, new Color(255 - col, col, 0), false);
-
-        RenderUtil.drawRoundedRect(this.x + 10, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT, this.x + (this.width - 10) * healthPerc, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT + this.height / 8, 2f, new Color(255 - col, col, 0));
+        switch (mode) {
+            case BULKY:
+                RenderUtil.drawRoundedRect(this.x, this.y, this.x + this.width, this.y + this.height, 2f, new Color(35, 35, 35));
+                Statics.getFontRenderer().drawString(this.killAuraModule.getCurrTarget().getName(), (int) (this.x + 10), (int) (this.y + 10), -1);
+                bloomBuffer.framebufferClear();
+                bloomBuffer.bindFramebuffer(true);
+                RenderUtil.drawRoundedRect(this.x + 10, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT, this.x + (this.width - 10) * healthPerc, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT + this.height / 8, 2f, new Color(255 - col, col, 0));
+                bloomBuffer.unbindFramebuffer();
+                RenderUtil.drawBloom(bloomBuffer.framebufferTexture, 15, 1, new Color(255 - col, col, 0), false);
+                RenderUtil.drawRoundedRect(this.x + 10, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT, this.x + (this.width - 10) * healthPerc, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT + this.height / 8, 2f, new Color(255 - col, col, 0));
+                break;
+            case GLASS:
+                bloomBuffer.framebufferClear();
+                bloomBuffer.bindFramebuffer(true);
+                RenderUtil.drawRect(this.x, this.y, this.x + this.width, this.y + this.height, new Color(255, 255, 255));
+                bloomBuffer.unbindFramebuffer();
+                RenderUtil.drawBloom(bloomBuffer.framebufferTexture, 15, 1, new Color(0, 0, 0), false);
+                Statics.getFontRenderer().drawString(this.killAuraModule.getCurrTarget().getName(), (int) (this.x + 10), (int) (this.y + 10), -1);
+                RenderUtil.drawRect(this.x + 10, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT, this.x + (this.width - 10) * healthPerc, this.y + 15 + Statics.getFontRenderer().FONT_HEIGHT + this.height / 8, new Color(255 - col, col, 0));
+                break;
+        }
     }
 
     @Override
@@ -96,5 +113,21 @@ public class TargetHUD extends Module {
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    private enum TargetHudMode implements ModeSetting.ModeTemplate {
+        BULKY("Bulky"),
+        GLASS("Glass");
+
+        private final String name;
+
+        TargetHudMode(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
     }
 }
